9e8a2dc27eab94946e78f1e9ddfe98ad1e8ab26b
Jason Chang <jasonchang0@berkeley.edu>
2018-12-21 08:03:08 -0800

    blur and smoothing

--- scripts/color_filtering.py
+++ scripts/color_filtering.py
@@ -27,9 +27,21 @@ while loop:
     mask = cv2.bitwise_or(mask1, mask2)
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
+    kernel = np.ones((15, 15), np.float32) / (15 * 15)
+    smooth = cv2.filter2D(res, -1, kernel=kernel)
+
+    blur = cv2.GaussianBlur(res, (15, 15), 0)
+    median = cv2.medianBlur(res, 15)
+    bilateral = cv2.bilateralFilter(res, 15, 75, 75)
+
     cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
+    # cv2.imshow('mask', mask)
     cv2.imshow('result', res)
+
+    # cv2.imshow('smooth', smooth)
+    # cv2.imshow('blur', blur)
+    # cv2.imshow('median', median)
+    # cv2.imshow('bilateral', bilateral)
 
     k = cv2.waitKey(1) & 0xFF
     loop = (k != ord('q')) and loop
